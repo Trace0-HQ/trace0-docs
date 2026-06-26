8199375dc2dd4c1c10e73d64609b9b6abb513803
davidcervi <davidcervi@users.noreply.github.com>
2026-06-26 07:09:56 +0000

    deploy: 7cc383a20bf1e4dac91d926990f738fce3b374bc

---
+++ _next/static/JSAUfMlRFd6B-kkc1xCFc/_buildManifest.js
@@ -0,0 +1,22 @@
+self.__BUILD_MANIFEST = {
+  "/404": [
+    "./static/chunks/41d8f590b58fcbb2.js"
+  ],
+  "/500": [
+    "./static/chunks/5c48299513401c62.js"
+  ],
+  "/_error": [
+    "./static/chunks/852f3995d28f2ce4.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/404",
+    "/500",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
